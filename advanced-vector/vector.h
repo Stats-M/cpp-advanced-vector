65ebaf6a2e4cf89fe33571d74a9faa622604541e
Stanislav <91995753+Stats-M@users.noreply.github.com>
2022-05-30 23:47:59 +0300

    Bug fixes

--- advanced-vector/vector.h
+++ advanced-vector/vector.h
@@ -428,6 +428,7 @@ public:
     template <typename... Args>
     iterator Emplace(const_iterator pos, Args&&... args)
     {
+        assert(pos >= begin() && pos <= end());
         // Отдельная обработка вставки в конец
         if (pos == cend())
         {
@@ -525,6 +526,8 @@ public:
 
     iterator Erase(const_iterator pos)   /*noexcept(std::is_nothrow_move_assignable_v<T>)*/
     {
+        assert(pos >= begin() && pos <= end());
+        
         // Вычисляем индекс вставляемого элемента в массиве
         size_t index = std::distance(cbegin(), pos);
 
